b94b4d171a3825793dcb116c028553e347ae14ee
David Ball <whatspaz@gmail.com>
2011-02-02 06:46:30 -0500

    added eclipse project

---
+++ src/main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author david
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("David Ball was here.");
+		
+		//now if you guys happen to get in here
+		//before i figure everything out, try to
+		//leave your name in a comment here
+		//don't delete anything in here, but add
+		//to it as you need to. --david
+
+	}
+
+}
