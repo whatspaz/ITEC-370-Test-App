bfaebd1127e259123317eb69aacda1c4546cf68e
David Ball <whatspaz@gmail.com>
2011-02-03 03:37:25 -0500

    stripped useless comments

--- src/main.java
+++ src/main.java
@@ -12,15 +12,7 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		System.out.println("David Ball was here.");
-		
-		//now if you guys happen to get in here
-		//before i figure everything out, try to
-		//leave your name in a comment here
-		//don't delete anything in here, but add
-		//to it as you need to. --david
 		
 		System.exit(0);
 
