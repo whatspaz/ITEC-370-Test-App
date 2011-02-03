acd21d70fd630957eee05f6d8492359711ff3525
Drew Salyer <spencer.salyer@gmail.com>
2011-02-03 10:21:33 -0800

    Edited file directly on Github website.

--- src/edu/radford/codebreakers/testing/Main.java
+++ src/edu/radford/codebreakers/testing/Main.java
@@ -14,7 +14,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("David Ball was here.");
-		
+		System.out.println("Drew edited this file online at Github.com");
+
 		System.exit(0);
 
 	}
