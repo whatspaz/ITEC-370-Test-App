93d2e4f3c2ce9f88d302221f36ccf3d2708c9d91
David Ball <whatspaz@gmail.com>
2011-03-24 20:09:34 -0400

    added a new line

--- src/main.java
+++ src/main.java
@@ -13,6 +13,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("David Ball was here.");
+		System.out.println("David Ball was here again on 3/24/2011.");
 		
 		System.exit(0);
 
